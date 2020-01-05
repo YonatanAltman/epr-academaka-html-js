19ea9714c172ed204c428db99536f1d1dca5e54e
Yonatan Altman <yonatan@yaltman.com>
2020-01-05 17:59:13 +0200

    validations

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -7,25 +7,66 @@ let person1 = {
     lastName: 'Redler',
     age: 36
 }
+let person2 = {
+    firstName: "Shirit",
+    lastName: 'Maooda',
+    age: 18
+}
+let cat = {
+    firstName: "Mew",
+    lastName: 'Bar-Zanav',
+    tail: true
+
+}
 arr.push(person1);
+arr.push(person2);
+arr.push(cat);
 
 document.getElementById('registerForm')
     .addEventListener("submit", function (e) {
-      
+
+
         e.preventDefault();
         let firstName = document.getElementById('firstName').value;
         let lastName = document.getElementById('lastName').value;
         let age = Number(document.getElementById('age').value);
-      
+        let password = document.getElementById('password').value;
+        let password2 = document.getElementById('password2').value;
+
         // let person = {
         //     firstName: firstName,
         //     lastName: lastName,
         //     age: age
         // };
-        let person = {firstName,lastName,age};
-        arr.push(person);
-        console.log(arr);
+        let a = 1;
+        let b = '1';
 
-    })
+        if (a === b) {
+
+        }
+        else {
+
+        }
+        // IF EVRITHING IS VALID
+        let person = { firstName, lastName, age };
+        arr.push(person);
+
+
+        for (const p of arr) {
+            console.log(getFullName(p));
+
+
+
+        }
+
+    });
+
+function getFullName(person) {
+    return person.firstName + ' ' + person.lastName;
+}
+
+function isPasswordsEqual(pass1,pass2) { 
+
+}
 
 
